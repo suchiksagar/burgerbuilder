4405378c8629c0a6a1d02834e220c5885cc230f2
suchiksagar <sagarksuchi@gmail.com>
2018-05-12 11:31:49 -0400

    added nav

---
+++ src/components/Navigation/NavigationItems/navigationItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const navigationItems = () => {
+    return (
+        <ul>
+            <li> <a href = "/"> A link </a> </li>
+        </ul>
+    );
+};
+
+export default navigationItems;
