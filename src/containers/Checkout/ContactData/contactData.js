f314e2a4af42f7b671d0100577fd349226cee8c7
suchiksagar <sagarksuchi@gmail.com>
2018-05-21 22:03:36 -0400

    added form submit and carefully submits customer info along with order

--- src/containers/Checkout/ContactData/contactData.js
+++ src/containers/Checkout/ContactData/contactData.js
@@ -8,12 +8,58 @@ import Input from '../../../components/ui/Input/input';
 class ContactData extends Component {
     
     state = {
-        name : '',
-        email: '',
-        address : {
-            street : '',
-            postalCode : ''
-        },
+        orderForm: {
+            name : {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street : {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'street'
+                },
+                value: ''
+            },
+            zipCode : {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip Code'
+                },
+                value: ''
+            },
+            country : {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email : {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },
+            deliveryMethod : {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value : 'fastest', displayValue: 'Fastest'},
+                        {value : 'cheapest', displayValue: 'Cheapest'},
+                    ]
+                },
+                value: ''
+            },
+        },       
         loading: false,
         purchased: false
     }
@@ -35,31 +81,51 @@ class ContactData extends Component {
             this.setState({loading : false, purchased: false});
           });
       }
+
+      inputChangedHandler = (event, inputIdentifier) => {
+            const updatedOrderForm = {
+                ...this.state.orderForm
+            } 
+            const updatedFormElement = {
+                ...updatedOrderForm[inputIdentifier]
+            }
+            updatedFormElement.value = event.target.value;
+            updatedOrderForm[inputIdentifier] = updatedFormElement;
+            this.setState({orderForm : updatedOrderForm});
+      };
     
       getOrderToSubmit () {
+        const formData= {};
+        for(let fromElementIdentifier in this.state.orderForm){
+            formData[fromElementIdentifier] = this.state.orderForm[fromElementIdentifier].value;
+        }
         return  {
           ingredients : this.props.ingredients,
           price : this.props.price,
-          customer : {
-            name : 'Bat Man',
-            address : {
-              street : 'Gotham City',
-              zipCode : '01701',
-              country : 'US'
-            },
-            email : 'somerandomemail@randomprovider.com'
-          },
+          customer : formData,
           deliveryMethod : 'quickiest'
         }
       }
 
     render () {
-        let form =  <form>
-                        <Input inputtype={"input"} type="text" name="name" placeholder="Your Name"  />
-                        <Input inputtype={"input"} type="email" name="email" placeholder="Your Email"  />
-                        <Input inputtype={"input"} type="text" name="street" placeholder="Your Street"  />
-                        <Input inputtype={"textarea"} type="text" name="postal" placeholder="Your Zip Code"  />
-                        <Button btnType="Success" clickHandler={this.orderHandler}> ORDER </Button>
+        const formElementsArray = [];
+        for(let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+        let form =  <form onSubmit={this.orderHandler}>                        
+                        {formElementsArray.map(formElement => (
+                           <Input
+                                key={formElement.id}
+                                elementType={formElement.config.elementType} 
+                                elementConfig={formElement.config.elementConfig}
+                                value={formElement.config.value}
+                                changed={(event) => this.inputChangedHandler(event, formElement.id)}
+                            /> 
+                        ))}
+                        <Button btnType="Success"> ORDER </Button>
                     </form>;
         if(this.state.loading && !this.state.purchased){
             form = <Spinner />;
